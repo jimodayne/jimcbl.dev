f8c8b0df06e1b749101fee6a3ce7d53cfcd5ebf4
Thinh Tran <tio212@gmail.com>
2024-06-23 14:11:52 +0300

    fix: responsive

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -10,10 +10,10 @@ const ProjectCard = (props: ProjectCardProps) => {
   return (
     <a target="_blank" href={url}>
       <div className="card bg-white min-h-28 border-slate-100 highlight-white dark:bg-slate-800 overflow-hidden dark:border-slate-500 ring-1 transition duration-300 ring-slate-900/5 shadow-xl rounded-xl mb-6 flex md:gap-4 flex-col md:flex-row">
-        <div className="left md:w-4/12 pt-[45%] relative border-b md:border-b-0">
+        <div className="left md:w-5/12 pt-[45%] md:pt-0  dark:border-slate-600 relative border-b md:border-b-0">
           <Image style={{ objectFit: 'cover', objectPosition: '0 0' }} src={image} alt="project" fill />
         </div>
-        <div className="right md:w-8/12 md:p-6 p-4">
+        <div className="right md:w-7/12 md:p-6 p-4">
           <p className="font-bold text-lg dark:text-white">{title}</p>
           <p className="py-2">{description}</p>
           <div>
--- src/components/SocialNetwork.tsx
+++ src/components/SocialNetwork.tsx
@@ -2,6 +2,7 @@ import { SOCIAL_NETWORK_LINKS, SocialNetworkType } from '@/app/props';
 import { IconType } from 'react-icons';
 import { FaLinkedin, FaGithub, FaTelegram, FaUnsplash, FaInstagram } from 'react-icons/fa';
 import cn from 'classnames';
+import { capitalize } from '@/utils';
 
 const socialIcons: Record<SocialNetworkType, IconType> = {
   linkedin: FaLinkedin,
@@ -24,10 +25,10 @@ export const SocialNetworkIcons = (props: SocialNetworkIconsProps) => {
     <div className={cn(className, 'socialNetworkIcons flex gap-4 py-2 flex-wrap', { isResponsive })}>
       {Object.entries(socialIcons).map(([key, Icon]) => {
         return (
-          <a key={key} target="_blank" href={SOCIAL_NETWORK_LINKS[key as SocialNetworkType]}>
+          <a key={key} title={capitalize(key)} target="_blank" href={SOCIAL_NETWORK_LINKS[key as SocialNetworkType]}>
             <Icon
               size={size}
-              className="transition dark:hover:text-white duration-300 hover:text-black dark:hover:opacity-100 hover:scale-110"
+              className="transition dark:text-white lg:dark:text-gray-300 lg:dark:hover:text-white duration-300 hover:text-black dark:hover:opacity-100 hover:scale-110"
             />
           </a>
         );
@@ -39,7 +40,10 @@ export const SocialNetworkIcons = (props: SocialNetworkIconsProps) => {
 const SocialNetwork = () => {
   return (
     <div className="social hidden lg:block py-4 mb-6">
-      <a href="mailto:jimtran98@gmail.com" className="mb-2 block transition duration-300 hover:opacity-60">
+      <a
+        href="mailto:jimtran98@gmail.com"
+        className="mb-2 block transition duration-300 dark:hover:text-white dark:hover:opacity-100 hover:opacity-60"
+      >
         jimtran98@gmail.com
       </a>
 
